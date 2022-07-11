4ae3695834d57cb17afd35fc0810abeaa1cd8e9a
rsk <hardboiled952@gmail.com>
2022-07-11 10:14:02 -0700

    +

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,9 +1,11 @@
-package java;
 
 public class Main
 {
     public static void main(String[] args)
     {
-
+        try
+        {
+            new Project_Challenge1();
+        } catch(Exception e) { e.printStackTrace(); System.exit(0); }
     }
 }
