2e92818ae8a1b9ea2f14ccbeecc6278e3325421d
JosephSpielman <106626253+JosephSpielman@users.noreply.github.com>
2022-07-12 14:05:29 -0500

    Group Testing with temp class
    --

--- src/java/JSTest.java
+++ src/java/JSTest.java
@@ -7,50 +7,42 @@ import java.util.Collections;
 
 public class JSTest {
     public static void main(String[] args) throws IOException {
-        //DataAccess.createInputFile();
-        //DataAccess.fillInputFile();
+
         Converter cvt = new Converter();
 
         ArrayList<String> readIn = new ArrayList<>(DataAccess.readInputFile());
         System.out.println("-----ReadIn----: "+readIn.size());
-        //readIn.stream().forEach(System.out::println);
+
         ArrayList<Integer> numList = new ArrayList<>();
 
 
-       // System.out.println(readIn.size());
-/*
+
+
         for (String s:readIn) {
-            numList.add(cvt.wordToNumber(s));
+            Integer temp = cvt.wordToNumber(s);
+            if(temp != null){
+                numList.add(cvt.wordToNumber(s));
+            }
         }
-*/
-        //int temp = cvt.wordToNumber("fifty one");
-        String temp = cvt.numberToWord(0);
-        System.out.println(temp);
 
-  //      numList.stream().forEach(System.out::println);
-        //Collections.sort(numList);
+/*
+        numList.stream().forEach(System.out::println);
+        Collections.sort(numList);
 
-        /*
+
         ArrayList<String> backToString = new ArrayList<>();
         for(int n:numList){
             backToString.add(cvt.numberToWord(n));
         }
 
-         */
-
-        //backToString.stream().forEach(System.out::println);
-        //numList.stream().forEach(System.out::println);
-
-//        ArrayList<String[]> afterColumn = new ArrayList<>(Project_Challenge1_v1.printColumns(backToString));
-        //afterColumn.stream().forEach(System.out::println);
-
-  //      DisplayData.displayData(afterColumn);
+        backToString.stream().forEach(System.out::println);
 
 
-        /*
-        ArrayList<String[]> getColumn = new ArrayList<>(Project_Challenge1_v1.printColumns(readIn));
+        ArrayList<String[]> afterColumn = new ArrayList<>(Project_Challenge1_v1.printColumns(backToString));
 
-        DisplayData.displayData(getColumn);
-        */
+        DisplayData.displayData(afterColumn);
+
+
+ */
     }
 }
